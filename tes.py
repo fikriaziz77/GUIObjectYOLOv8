46c29db20e5e6405660604e83e8c9f2cd7daf9fd
fikriaziz77 <fikri.official77@gmail.com>
2024-04-01 03:05:24 +0700

    update 1Apr

--- tes.py
+++ tes.py
@@ -1,6 +1,6 @@
 import cv2
 
-frame = cv2.imread('assets/background.jpg')
+frame = cv2.imread('assets/background.png')
 cv2.circle(frame, (600,200),3, (0,255,0), 3)
 cv2.circle(frame, (386,306),3, (0,255,255), 3)
 
